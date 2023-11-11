f37a03af587b2904c689f42e70216fba382a4846
lopentus <ososkov2001@gmail.com>
2023-11-11 21:01:24 +0300

    fix docker and local start

---
+++ README.md
@@ -0,0 +1,86 @@
+# Kittygram
+
+### App description
+
+## Used technologies:
+#### Django
+#### Django REST
+#### React
+
+## Start the project:
+
+### Clone the repository
+```bash
+git clone 
+```
+
+### Create .env file with the following script
+```bash
+cd kittygram/
+echo '''DB_NAME=kittygram_db
+POSTGRES_USER=kittygram_user
+POSTGRES_PASSWORD=kittygram_password
+DB_HOST=localhost
+DB_PORT=5432
+DJANGO_SECRET_KEY=your_secret_key
+DJANGO_DEBUG=True
+DJANGO_ALLOWED_HOSTS=127.0.0.1 localhost''' > .env
+```
+
+# Run project in docker:
+
+```bash
+docker compose up -d
+```
+#### wait some time to let docker build containers
+
+#### logs:
+```bash
+docker logs <container_name> -f
+```
+
+#### stop containers:
+```bash
+docker compose down
+```
+
+# Run locally:
+
+## Start backend
+
+### postgresql
+```bash
+sudo su postgres
+psql
+CREATE DATABASE kittygram_db;
+CREATE USER kittygram_user WITH PASSWORD 'kittygram_password';
+GRANT ALL PRIVILEGES ON DATABASE kittygram_db TO kittygram_user;
+ALTER USER kittygram_user CREATEDB;
+```
+
+### Create venv and install requirements.txt
+```bash
+cd backend/
+python -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+### Run migrations and start server
+```bash
+python manage.py migrate
+python manage.py runserver
+```
+
+### Optionally create superuser
+```bash
+python manage.py createsuperuser
+```
+
+## Start frontend
+```bash
+cd frontend/
+npm install
+npm start
+```
---
+++ backend/kittygram_backend/settings.py
@@ -0,0 +1,126 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
+
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(' ')
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework.authtoken',
+    'rest_framework',
+    'corsheaders',
+    'djoser',
+    'cats.apps.CatsConfig',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'kittygram_backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', default='kittygram_db'),
+        'USER': os.getenv('POSTGRES_USER', default='kittygram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='kittygram_password'),
+        'HOST': os.getenv('DB_HOST', default='localhost'),
+        'PORT': os.getenv('DB_PORT', default=5432),
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+)
